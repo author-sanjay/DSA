d2f505c304d33191206a52faec95c51185fc50a7
sanjay <sanjaykumar73189@gmail.com>
2022-06-28 21:45:55 +0530

    assignment 23 doing

---
+++ Assignment 23/encryption.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'encryption' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string encryption(string s) {
+int n=s.size();
+int jump=sqrt(n);
+if(jump*jump!=n){
+    jump++;
+}
+string ans="";
+for(int i=0;i<jump;i++){
+    for(int j=i;j<n;j+=jump){
+        ans+=s[j];
+    }
+    ans+=" ";
+}
+return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = encryption(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
