772a3e3773fdcd2cf1e6c37e2a79ec9021ce6250
Sanjay Kumar <sanjaykumar73189@gmail.com>
2022-12-12 16:27:15 +0530

    Update 0746-min-cost-climbing-stairs.cpp

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -15,10 +15,31 @@ public:
         dp[n]= (min(solve(cost,n-1,dp),solve(cost,n-2,dp))+cost[n]);
         return dp[n];
     }
+    
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
         vector<int>dp(n+1,-1);
         return min(solve(cost,n-1,dp),solve(cost,n-2,dp));
         
+    }
+};
+
+
+OR
+class Solution {
+public:
+    int solve(vector<int> cost,int n){
+        vector<int>dp(n+1,-1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return min(dp[n-1],dp[n-2]);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        return solve(cost,n);
     }
 };
